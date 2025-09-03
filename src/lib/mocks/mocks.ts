fb58987e912547ec1fd9e1c4aa3f4364fc56d516
.d[i]w <77813468+W1ml@users.noreply.github.com>
2025-09-03 11:37:29 +0300

    Homework_1
    Вынес моки в отдельный файл /src/lib/mocks/mocks.ts и добавил стили через /src/shared/layouts/MainLayout.module.css и import

---
+++ src/lib/mocks/mocks.ts
@@ -0,0 +1,12 @@
+export const mockPosts : {id : number; title: string; body: string }[] = [
+    {
+        id: 1,
+        title: 'Пост номер один',
+        body: 'Описание первого поста'
+    },
+    {
+        id: 2,
+        title: 'Пост номер 2',
+        body: 'Описание второго поста'
+    },
+]
---
+++ src/shared/layouts/MainLayout.tsx
@@ -0,0 +1,18 @@
+import { Header } from '../../widgets/LayoutHeader/Header.tsx'
+import { Footer } from '../../widgets/LayoutFooter/Footer.tsx'
+import './MainLayout.module.css'
+import React from "react";
+
+interface MainLayoutProps {
+    children: React.ReactNode
+}
+
+export const MainLayout = ({ children }: MainLayoutProps) => {
+    return (
+        <div className="layout">
+            <Header />
+            <main>{children}</main>
+            <Footer />
+        </div>
+    )
+}
